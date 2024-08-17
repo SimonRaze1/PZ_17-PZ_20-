b9a33014072174aff5e66d11dd072cb3a0b4b6d5
Даниил Широков <daniil.shirokov.19042006@gmail.com>
2024-08-17 17:14:57 +0500

    Добавьте файлы проекта.

---
+++ PZ_17/Fridge1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PZ_17
+{
+    public class Fridge1 : ICloneable, IComparable
+    {
+        // Поля класса
+        public string manufacturer;
+        public string model;
+        public int yearOfRelease;
+        public string malfunction;
+        public string owner;
+
+        // Метод для расчета стоимости ремонта
+        public double RepairCost()
+        {
+            // Пример расчета стоимости ремонта, 
+            // в реальной ситуации логика расчета может быть сложнее
+            double cost = 0;
+            if (malfunction == "Неисправен компрессор")
+            {
+                cost = 5000;
+            }
+            else if (malfunction == "Повреждена дверца")
+            {
+                cost = 2000;
+            }
+            else if (malfunction == "Не работает подсветка")
+            {
+                cost = 1000;
+            }
+            return cost;
+        }
+
+        // Метод для вывода информации о холодильнике
+        public void PrintInfo()
+        {
+            Console.WriteLine($"Производитель: {manufacturer}");
+            Console.WriteLine($"Модель: {model}");
+            Console.WriteLine($"Год выпуска: {yearOfRelease}");
+            Console.WriteLine($"Неисправность: {malfunction}");
+            Console.WriteLine($"Владелец: {owner}");
+            Console.WriteLine($"Стоимость ремонта: {RepairCost():C}");
+        }
+        public object Clone()
+        {
+            Fridge1 newFridge = new Fridge1();
+            newFridge.manufacturer = manufacturer;
+            newFridge.model = model;
+            newFridge.yearOfRelease = yearOfRelease;
+            newFridge.malfunction = malfunction;
+            newFridge.owner = owner;
+            return newFridge;
+        }
+        public int CompareTo(object obj)
+        {
+            if (obj == null) return 1;
+            Fridge1 otherFridge = obj as Fridge1;
+            if (otherFridge != null)
+            {
+                // Сравнение по году выпуска
+                return yearOfRelease.CompareTo(otherFridge.yearOfRelease);
+            }
+            else
+            {
+                throw new ArgumentException("Объект не является Fridge.");
+            }
+        }
+    }
+}
---
+++ PZ_17/Main.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PZ_17
+{
+    public class MainKlass
+    {
+        public static void Main(string[] args)
+        {
+            // Создание объектов класса Fridge
+            Fridge1 fridge1 = new Fridge1
+            {
+                manufacturer = "Samsung",
+                model = "RB38T776FSR",
+                yearOfRelease = 2022,
+                malfunction = "Неисправен компрессор",
+                owner = "Иван Иванов"
+            };
+
+            Fridge1 fridge2 = new Fridge1
+            {
+                manufacturer = "LG",
+                model = "GN-L205BL",
+                yearOfRelease = 2019,
+                malfunction = "Повреждена дверца",
+                owner = "Петр Петров"
+            };
+
+            Fridge1 fridge3 = new Fridge1
+            {
+                manufacturer = "Bosch",
+                model = "KGN39VL30",
+                yearOfRelease = 2020,
+                malfunction = "Не работает подсветка",
+                owner = "Мария Сидорова"
+            };
+
+            // Сортировка холодильников по году выпуска
+            Fridge1[] fridges = { fridge1, fridge2, fridge3 };
+            Array.Sort(fridges);
+
+            // Вывод информации о холодильниках
+            Console.WriteLine("Отсортированные холодильники:");
+            foreach (Fridge1 fridge in fridges)
+            {
+                fridge.PrintInfo();
+                Console.WriteLine("----------------------");
+            }
+        }
+    }
+}
